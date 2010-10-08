5089b35a7596cbd1ef3350d38dc27c24c6be6fb8
David Pick <pickdavid@gmail.com>
2010-10-08 01:20:43 -0400

    minor refactoring

--- server/server.rb
+++ server/server.rb
@@ -1,14 +1,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'config/init'
+Dir['routes/*'].each { |obj| require obj }
 
 configure do
   API_KEY = 5500
   OBJECTS = ['user', 'point', 'trail', 'condition', 'category']
   User.first_or_create(:email => 'test@brousalis.com', :pwhash => Digest::SHA1.hexdigest('password'))
 end
-
-OBJECTS.each { |obj| require "routes/#{obj}_routes" }
 
 before do
   OBJECTS.each do |object|

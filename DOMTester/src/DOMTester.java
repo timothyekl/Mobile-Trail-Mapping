aef67402e04b4604930034952245adb2608f0123
Eric Stokes <fernferret@gmail.com>
2010-10-20 22:29:04 -0400

    Refactored the DOMTester, preparing for merge into the Android code

--- DOMTester/src/DOMTester.java
+++ DOMTester/src/DOMTester.java
@@ -17,18 +17,16 @@ public class DOMTester {
 
 	static String XML_FILE = "http://www.fernferret.com/samplexml.xml";
 	static String XML_SCHEMA = "http://www.fernferret.com/mtmSchema.xsd";
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		StreamSource xmlSource = new StreamSource(XML_FILE);
-		//factory.setNamespaceAware(true);
+
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		//factory.setValidating(true);
+
 		DocumentBuilder builder = null;
 		
-		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);		
+		SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		
+		Document doc = null;
 		try {
 			URL schemaDoc = new URL(XML_SCHEMA);
 			URL xmlDoc = new URL(XML_FILE);
@@ -36,68 +34,56 @@ public class DOMTester {
 			factory.setSchema(schema);
 			Validator validator = schema.newValidator();
 			validator.validate(xmlSource);
-			//validator.validate(new StreamSource("samplexml.xml"));
-			builder = factory.newDocumentBuilder();
-			System.out.println(builder.isValidating());
-			Document doc = builder.parse(new InputSource(xmlDoc.openStream()));
 
-			processDocument(doc);
+			builder = factory.newDocumentBuilder();
+			doc = builder.parse(new InputSource(xmlDoc.openStream()));
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		extractTrails(doc);
 	}
 
-	private static void processDocument(Document doc) {
-		NodeList l = doc.getElementsByTagName("trail");
-		int i = 0;
-		Node currentNode = l.item(i);
-		NodeList points = null;
-		Node point = null;
-		Node localPoint = null;
+	private static void extractTrails(Document doc) {
+		NodeList itemList = doc.getElementsByTagName("trail");
+		Node currentNode = itemList.item(0);
 		while (currentNode != null) {
-
-			System.out.println("Node Name : " + currentNode.getNodeName());
-			System.out.println("Node ID   : "
-					+ currentNode.getAttributes().getNamedItem("id")
-							.getNodeValue());
-			System.out.println("Node Type : " + currentNode.getNodeType());
-			System.out.println("Node Value: " + currentNode.getNodeValue());
-			points = currentNode.getChildNodes().item(1).getChildNodes();
-			int j = 0;
-			point = points.item(j);
-			while (point != null) {
-				if (point.getNodeType() == Node.ELEMENT_NODE) {
-					System.out.println("Point       : "
-							+ point.getNodeName());
-					System.out.println("Point ID    : "
-							+ point.getAttributes().getNamedItem("id").getNodeValue());
-					// while(point != null) {
-
-					// }
-					localPoint = point.getFirstChild().getNextSibling();
-					while(localPoint != null) {
-						if(localPoint.getNodeType() == Node.ELEMENT_NODE && localPoint.getNodeName() != "connections") {
-							System.out.println(localPoint.getNodeName() + " : " + localPoint.getTextContent());
-						}
-						if(localPoint.getNodeName() == "connections") {
-							localPoint = localPoint.getFirstChild();
-						}
-						else {
-							localPoint = localPoint.getNextSibling();
-						}
-					}
-				}
-				j++;
-				point = points.item(j);
+			if(currentNode.getNodeType() == Node.ELEMENT_NODE) {
+				System.out.println("Trail: ");
+				extractTrail(currentNode);
 			}
-			System.out.println();
-			// System.out.println("Node" + l.)
-			i++;
-			currentNode = l.item(i);
+			currentNode = currentNode.getNextSibling();
+		}
+	}
+
+	private static void extractTrail(Node currentNode) {
+		Node point = null;
+		point = currentNode.getFirstChild().getNextSibling().getFirstChild().getNextSibling();
+		while (point != null) {
+			if (point.getNodeType() == Node.ELEMENT_NODE) {
+				getPointInfo(point);
+			}
+			point = point.getNextSibling();
 		}
 		System.out.println();
 	}
 
+	private static void getPointInfo(Node point) {
+		Node localPoint = null;
+		System.out.println("Point       : " + point.getNodeName());
+		System.out.println("Point ID    : " + point.getAttributes().getNamedItem("id").getNodeValue());
+		
+		localPoint = point.getFirstChild().getNextSibling();
+		while(localPoint != null) {
+			if(localPoint.getNodeType() == Node.ELEMENT_NODE && localPoint.getNodeName() != "connections") {
+				System.out.println(localPoint.getNodeName() + " : " + localPoint.getTextContent());
+			}
+			if(localPoint.getNodeName() == "connections") {
+				localPoint = localPoint.getFirstChild();
+			}
+			else {
+				localPoint = localPoint.getNextSibling();
+			}
+		}
+		System.out.println();
+	}
 }

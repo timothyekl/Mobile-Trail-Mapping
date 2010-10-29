f2d07d0b024df3df83618816e51f22be4b328596
Eric Stokes <fernferret@gmail.com>
2010-10-28 22:20:59 -0400

    Added Comments to BetaDialog

--- Android/src/com/brousalis/BetaDialog.java
+++ Android/src/com/brousalis/BetaDialog.java
@@ -4,6 +4,13 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.Window;
 
+/**
+ * BetaDialog is a custom extension of the Dialog class that
+ * will allow us to show any popups related to the android
+ * beta program.
+ * @author Eric Stokes 10/28/2010
+ *
+ */
 public class BetaDialog extends Dialog {
 	public BetaDialog(Context context, int xmlToLoad) {
 		super(context);

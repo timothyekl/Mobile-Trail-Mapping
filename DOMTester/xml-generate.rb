c4b733ecb290949f3c17dea75e783d000cd5494e
David Pick <pickdavid@gmail.com>
2010-11-30 19:13:50 -0500

    ruby version of xml-generate does same thing as python version now

--- DOMTester/xml-generate.rb
+++ DOMTester/xml-generate.rb
@@ -1,5 +1,5 @@
 File.open("points.xml", 'w') do |out_file|
-  out_file.write('<?xml version="1.0" encoding="ISO-8859-1"?><data><trails><trail id="0" name="Heritage"><points>\n')
+  write_header(out_file)
 
   in_file = File.new("points.txt", 'r')
   count = 0
@@ -8,7 +8,31 @@ File.open("points.xml", 'w') do |out_file|
     parts = line.split(" ").map { |part| part.chomp }
     lat = parts[0]
     long = parts[1]
-
-    out_file.write("<point id='#{count}'><title>Point #{count}</title><description>Point #{count}</description><category id="0">Category</category><condition>Open</condition><latitude>#{lat}</latitude><longitude>#{long}</longitude>")
+    write_point(lat, long, count, out_file)
+    write_connection(out_file, count - 1) if count > 0
+    close_point_tag(out_file)
+    count += 1
   end
+
+  close_tags(out_file)
+end
+
+def write_point(lat, long, id, file)
+    file.write("<point id='#{id}'><title>Point #{id}</title><description>Point #{id}</description><category id="0">Category</category><condition>Open</condition><latitude>#{lat}</latitude><longitude>#{long}</longitude>")
+end
+
+def write_connection(file, id)
+  file.write("<connections><connection>#{id}</connection></connections>")
+end
+
+def write_header(file)
+  file.write('<?xml version="1.0" encoding="ISO-8859-1"?><data><trails><trail id="0" name="Heritage"><points>\n')
+end
+
+def close_tags(file)
+  file.write("</points></trail></trails><misc></misc></data>\n")
+end
+
+def close_point_tag(file)
+  file.write("</point>\n")
 end
